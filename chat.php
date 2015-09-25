6b36ba0e2fe26a97b2b6dced96b965efe5d8646d
dj-monstercat <jordansim189189@gmail.com>
2015-09-24 22:41:43 -0500

    Update chat.php

--- chat.php
+++ chat.php
@@ -1,8 +1,33 @@
-<div id="chat-messages" style="height: 257px;"><div class="cm log"><div class="msg"><div class="text cid-undefined">Welcome to plug.dj. Version 1.4.21.9360</div></div></div><div class="cm welcome"><div class="msg"><div class="text cid-undefined">Welcome to my community DJ sim  official<span class="emoji-outer emoji-sizer"><span class="emoji-inner" style="background-position:5.882352941176471% 100%;background-size:3500%"></span></span>. Please fap to the rules, if you feel like it. (No seriously, plz do, @confibula gets turned on by it)</div></div></div></div>
-<div id="chat-input">
-    <form class="chat-input-form" onsubmit="return false">
-        <input id="chat-input-field" type="text" value="" placeholder="Click here to join the conversation" maxlength="256">
-    </form>
-</div>
-<div id="chat-suggestion"><div class="frame-background"></div>
-<div id="chat-suggestion-items"></div></div>
+element.style {
+    height: 257px;
+}
+#chat-messages {
+    position: absolute;
+    top: 227px;
+    left: 0;
+    overflow-x: hidden;
+    overflow-y: auto;
+    width: 100%;
+    color: #eee;
+    font-size: 12px;
+}
+#chat-input-field, #chat-messages {
+    font-family: Roboto,sans-serif;
+}
+#chat-header, #chat-messages {
+    border-bottom: #1c1f25 1px solid;
+}
+*, .jspArrow {
+    margin: 0;
+    padding: 0;
+}
+user agent stylesheetdiv {
+    display: block;
+}
+Inherited from body
+body {
+    font-family: 'Open Sans',sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+    background: #000;
+    color: #eee;
