958e04c2d7cebabcd0497cb9a669877bdfe4e930
dj-monstercat <jordansim189189@gmail.com>
2015-09-24 22:35:10 -0500

    Create chat.php

---
+++ chat.php
@@ -0,0 +1,8 @@
+<div id="chat-messages" style="height: 257px;"><div class="cm log"><div class="msg"><div class="text cid-undefined">Welcome to plug.dj. Version 1.4.21.9360</div></div></div><div class="cm welcome"><div class="msg"><div class="text cid-undefined">Welcome to my community DJ sim  official<span class="emoji-outer emoji-sizer"><span class="emoji-inner" style="background-position:5.882352941176471% 100%;background-size:3500%"></span></span>. Please fap to the rules, if you feel like it. (No seriously, plz do, @confibula gets turned on by it)</div></div></div></div>
+<div id="chat-input">
+    <form class="chat-input-form" onsubmit="return false">
+        <input id="chat-input-field" type="text" value="" placeholder="Click here to join the conversation" maxlength="256">
+    </form>
+</div>
+<div id="chat-suggestion"><div class="frame-background"></div>
+<div id="chat-suggestion-items"></div></div>
